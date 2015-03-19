1e1d49e10ea4500fe2a0f0fce1f22fa18113136e
thepule <pule.astro@gmail.com>
2015-03-20 00:00:28 +0100

    Modified coursera assignment, v1.0

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file was created in order to solve the Assignment number 2 of the
+## rprog-012 Coursera course : "R Programming", 2015.
+## The following 2 functions create a special object that stores a matrix
+## and chaches its inverse.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix receives a matrix as input and
+## produces a list of functions as an output. Details on the
+## behaviours of the functions follow as comments.
 
 makeCacheMatrix <- function(x = matrix()) {
+   invmatrix <- NULL
+   # set() re-assigns a new matrix to x and re-initialize invmatrix
+   set <- function(y){
+     x <<- y
+     invmatrix <- NULL
+   }
+   # get() outputs the initial matrix
+   get <- function() x
+   # setInv() assigns the inverse matrix to invmatrix
+   setInv <- function(inverse) invmatrix <<- inverse
+   # getInv() outputs the inverse matrix
+   getInv <- function() invmatrix
+   list(set = set, get = get,
+       setInv = setInv, getInv = getInv)
+   }
 
-}
 
-
-## Write a short comment describing this function
+## The following functions receives the output 
+## of makeCacheMatrix and checks if the inverse matrix is already
+## stored in invmatrix. If it is, it returns it. Otherwise, it calculates
+## the inverse, stores it in invmatrix and outputs the inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   invmatrix <- x$getInv()
+   if(!is.null(invmatrix)) {
+      message("getting cached data (inverse matrix)")
+      return(invmatrix)
+   }
+   ## Assignment of the matrix to data via the get() function
+   ## defined in makeCacheMatrix
+   data <- x$get()
+   ## Inverse calculation
+   invmatrix <- solve(data)
+   ## Assignment of the inverse to invmatrix
+   x$setInv(invmatrix)
+   invmatrix
 }
